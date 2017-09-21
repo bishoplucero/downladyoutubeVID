659793ff0e08292390e46279279e1f17d141e185
bishop <32101733+ARDragonite@users.noreply.github.com>
2017-09-21 16:31:09 -0500

    Update downloadytVID.py

--- downloadytVID.py
+++ downloadytVID.py
@@ -1,15 +1,3 @@
-# Hello, Bishop. This is David speaking.
-# From whatever means necessary,
-# I want you to code a program that downloads a video
-# from youtube.
-# HINTS:
-# Stackoverflow and StackExchange
-# youtube-dl
-# pip3 install <program>
-#
-# Good luck my dude.
-# Code the code underneath here and upload it to your
-# GitHub page when you complete and just tell me!
 
 import youtube_dl #library
 
